1ae7fd2d2fe3b9683c5f5cc9d975a2d63deafe0f
Jessica <jessica.paraal@gmail.com>
2022-03-15 13:36:42 +0100

    Primer commit

---
+++ Git1/src/clase/Alumno.java
@@ -0,0 +1,69 @@
+package clase;
+
+public class Alumno {
+
+	
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private String id;
+	
+	
+	public Alumno(String nombre, String apellido, int edad, String id) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.id = id;
+	}
+
+
+	public Alumno() {
+		super();
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+}
